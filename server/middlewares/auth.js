a0f8264adbb83d8de1ccd2eafde73c0b76836f32
Gaurang <gaurangpande68@gmail.com>
2022-11-04 01:45:44 +0530

    Conflicts Resolved

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,43 +1,25 @@
 const admin = require("../firebase");
 
-
-exports.authCheck = async (req,res,next) =>{
-
-<<<<<<< HEAD
-
-=======
-   try{
-        
-    
->>>>>>> 492d334108abd7ddbcc61d50a3400ada3ca709ca
-    const email =  req.body.email;
-    const password =     req.body.password;
+exports.authCheck = async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
     req.credential = {
-     email,password
-    }
-   try{
-   
+      email,
+      password,
+    };
 
     next();
-   }
-   catch(err)
-   {
-       res.status(401).json({err: "Invalid Request"});
-   }
+  } catch (err) {
+    res.status(401).json({ err: "Invalid Request" });
+  }
 };
 
-
-exports.profileCheck = (req,res,next) =>{
-
-    console.log(req.body);
-    try{
-         
-      
-        next();
-    }
-    catch(err)
-    {
-        res.status(401).json({err: "Invalid Request"});
-    }
- };
- 
+exports.profileCheck = (req, res, next) => {
+  console.log(req.body);
+  try {
+    next();
+  } catch (err) {
+    res.status(401).json({ err: "Invalid Request" });
+  }
+};
