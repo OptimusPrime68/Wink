491b21ad289ed6090e5acfa019368b061a99e4e0
Gaurang <gaurangpande68@gmail.com>
2022-11-04 19:38:23 +0530

    Payment Page

--- winknlink/src/components/Pages/Photo.js
+++ winknlink/src/components/Pages/Photo.js
@@ -11,142 +11,146 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-
 const locales = {
-    "en-US": require("date-fns/locale/en-US"),
+  "en-US": require("date-fns/locale/en-US"),
 };
 const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
 });
 
 const events = [
-    {
-        title: "Big Meeting",
-        allDay: true,
-        start: new Date(2021, 6, 0),
-        end: new Date(2021, 6, 0),
-    },
-    {
-        title: "Vacation",
-        start: new Date(2021, 6, 7),
-        end: new Date(2021, 6, 10),
-    },
-    {
-        title: "Conference",
-        start: new Date(2021, 6, 20),
-        end: new Date(2021, 6, 23),
-    },
+  {
+    title: "Big Meeting",
+    allDay: true,
+    start: new Date(2021, 6, 0),
+    end: new Date(2021, 6, 0),
+  },
+  {
+    title: "Vacation",
+    start: new Date(2021, 6, 7),
+    end: new Date(2021, 6, 10),
+  },
+  {
+    title: "Conference",
+    start: new Date(2021, 6, 20),
+    end: new Date(2021, 6, 23),
+  },
 ];
 
 const Photo = () => {
+  const [book, setBook] = useState({
+    name: "The Fault In Our Stars",
+    author: "John Green",
+    img: "https://images-na.ssl-images-amazon.com/images/I/817tHNcyAgL.jpg",
+    price: 250,
+  });
 
-	const [book, setBook] = useState({
-		name: "The Fault In Our Stars",
-		author: "John Green",
-		img: "https://images-na.ssl-images-amazon.com/images/I/817tHNcyAgL.jpg",
-		price: 250,
-	});
+  const initPayment = (data) => {
+    const options = {
+      key: "rzp_test_yWZnCopCzsa76e",
+      amount: data.amount,
+      email: "piyushjaiswal380@gmail.com",
+      currency: data.currency,
+      name: book.name,
+      description: "Test Transaction",
+      image: book.img,
+      order_id: data.id,
+      handler: async (response) => {
+        response["email"] = "piyushjaiswal380@gmail.com";
+        response["amount"] = data.amount;
+        response["tenure"] = 3;
 
-	const initPayment = (data) => {
-		const options = {
-			key: "rzp_test_yWZnCopCzsa76e",
-			amount: data.amount,
-      email:"piyushjaiswal380@gmail.com",
-			currency: data.currency,
-			name: book.name,
-			description: "Test Transaction",
-			image: book.img,
-			order_id: data.id,
-			handler: async (response) => {
-
-        response['email'] = "piyushjaiswal380@gmail.com";
-        response['amount'] = data.amount;
-        response['tenure'] = 3;
-
-				try {
-					const verifyUrl = "http://localhost:4000/api/verify";
-					const { data } = await axios.post(verifyUrl,response);
-					console.log(data);
-				}catch (error) {
-					console.log(error);
-				}},
-			theme: {
-				color: "#3399cc",
-			},
-		};
-		const rzp1 = new window.Razorpay(options);
-		rzp1.open();
-	};
-
-	const handlePayment = async () => {
-		try {
-			const orderUrl = "http://localhost:4000/api/order";
-			const { data } = await axios.post(orderUrl, { amount: book.price});
-			console.log(data);
-			initPayment(data.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-    const [allEvents, setAllEvents] = useState(events);
-
-	const handleAddEvent = (e)=>{
-		for (let i=0; i<allEvents.length; i++){
-
-            const d1 = new Date (allEvents[i].start);
-            const d2 = new Date(newEvent.start);
-            const d3 = new Date(allEvents[i].end);
-            const d4 = new Date(newEvent.end);
-         
-			console.log(d1 + d2 + d3 + d4);            
-
-             if (
-              ( (d1  <= d2) && (d2 <= d3) ) || ( (d1  <= d4) &&
-                (d4 <= d3) )
-              )
-            {   
-                console.log("Clash");
-                break;
-             }
-    
+        try {
+          const verifyUrl = "http://localhost:4000/api/verify";
+          const { data } = await axios.post(verifyUrl, response);
+          console.log(data);
+        } catch (error) {
+          console.log(error);
         }
-        
-        
-        setAllEvents([...allEvents, newEvent]);
-	}
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    const rzp1 = new window.Razorpay(options);
+    rzp1.open();
+  };
 
+  const handlePayment = async () => {
+    try {
+      const orderUrl = "http://localhost:4000/api/order";
+      const { data } = await axios.post(orderUrl, { amount: book.price });
+      console.log(data);
+      initPayment(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
 
-  
+  const handleAddEvent = (e) => {
+    for (let i = 0; i < allEvents.length; i++) {
+      const d1 = new Date(allEvents[i].start);
+      const d2 = new Date(newEvent.start);
+      const d3 = new Date(allEvents[i].end);
+      const d4 = new Date(newEvent.end);
 
-return(
-  <>
-  <p>{book.price}</p>
-  <button onClick={handlePayment}>Buy Now</button>
+      console.log(d1 + d2 + d3 + d4);
 
+      if ((d1 <= d2 && d2 <= d3) || (d1 <= d4 && d4 <= d3)) {
+        console.log("Clash");
+        break;
+      }
+    }
 
-  <h1>Calendar</h1>
-            <h2>Add New Event</h2>
-            <div>
-                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-				<DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
-                <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
-                <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
-                    Add Event
-                </button>
-                
-		    </div>
-			<Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
-  </>
-)
+    setAllEvents([...allEvents, newEvent]);
+  };
 
-}
+  return (
+    <>
+      <p>{book.price}</p>
+      <button onClick={handlePayment}>Buy Now</button>
 
+      <h1>Calendar</h1>
+      <h2>Add New Event</h2>
+      <div>
+        <input
+          type="text"
+          placeholder="Add Title"
+          style={{ width: "20%", marginRight: "10px" }}
+          value={newEvent.title}
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+        />
+        <DatePicker
+          placeholderText="Start Date"
+          style={{ marginRight: "10px" }}
+          selected={newEvent.start}
+          onChange={(start) => setNewEvent({ ...newEvent, start })}
+        />
+        <DatePicker
+          placeholderText="End Date"
+          selected={newEvent.end}
+          onChange={(end) => setNewEvent({ ...newEvent, end })}
+        />
+        <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+          Add Event
+        </button>
+      </div>
+      <Calendar
+        localizer={localizer}
+        events={allEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "50px" }}
+      />
+    </>
+  );
+};
 
 export default Photo;
