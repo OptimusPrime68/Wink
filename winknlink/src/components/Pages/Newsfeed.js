2579cea7d47d68c59feff22319eecd747d1834a1
Gaurang <gaurangpande68@gmail.com>
2022-11-08 22:55:18 +0530

    NewsFeed Tooltip added

--- winknlink/src/components/Pages/Newsfeed.js
+++ winknlink/src/components/Pages/Newsfeed.js
@@ -3,7 +3,7 @@ import { Button } from "react-bootstrap";
 import Header from "./Header";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
-import { Avatar, IconButton } from "@mui/material";
+import { Avatar, IconButton, Tooltip } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import BottomDrawer from "./BottomDrawer";
 import "../styles/Newsfeed.css";
@@ -26,7 +26,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import Heart from "react-heart";
 import { toast } from "react-toastify";
 
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import CardMedia from "@mui/material/CardMedia";
@@ -61,11 +61,10 @@ function Newsfeed() {
   const handleClose = () => setOpen(false);
   const [loading, setLoading] = useState(false);
   let { user } = useSelector((state) => ({ ...state }));
-  const [profileD,setProfileD] = useState({});
-  const [profile,setProfile] = useState(0);
-  const [flag,setFlag] = useState(true);
+  const [profileD, setProfileD] = useState({});
+  const [profile, setProfile] = useState(0);
+  const [flag, setFlag] = useState(true);
   const dispatch = useDispatch();
-
 
   const counter = useRef(0);
   const handleLoad = () => {
@@ -76,143 +75,109 @@ function Newsfeed() {
 
   useEffect(() => {
     setLoading(true);
-  axios.post("http://localhost:4000/api/get-profile-id",{email:user.email}).then((data)=>{
+    axios
+      .post("http://localhost:4000/api/get-profile-id", { email: user.email })
+      .then((data) => {
+        console.log(data.data);
+        const id = data.data.id._id;
+        console.log(id);
+        setProfile(id);
+        setProfileD(data.data.id);
 
-     
-
-      console.log(data.data);
-      const id = data.data.id._id;
-      console.log(id);
-      setProfile(id);
-      setProfileD(data.data.id);
-      
-     
-
-     axios.post("http://localhost:4000/api/get-all-post",{authorid:id,email:user.email}).then((data)=>{
-  
-      console.log(data);
-
-      if(data.data.length == 0)
-      setLoading(false);
-
-      data.data.map((e)=>{
-
-
-
-      const id = e._id;
-      const ex = e.authorId.email;
-      const imageListRef = ref(storage, `${ex}/post/${id}`);
-      e['files'] = [];
-      setPost((prev) => [...prev, e]);
-      listAll(imageListRef)
-      .then((response) => {
-        console.log(e.content,response);
-
-        if(response.items.length == 0)
-        setLoading(false);
-      
-        response.items.forEach((item) => {
-          getDownloadURL(item).then((url) => {
-            
-            setLoading(false);
-      
-
-           setPost(current=>current.map(obj=>{
-
-            
-             if(obj == e){
-             obj.files = [...obj.files,url]; 
-             }
-             return obj;
-             
-           }))
+        axios
+          .post("http://localhost:4000/api/get-all-post", {
+            authorid: id,
+            email: user.email,
           })
-      })})
-    })}).catch((error)=>{
-      console.log(error);
-    })
-  }).catch((error)=>console.log(error));
-  },[])
+          .then((data) => {
+            console.log(data);
 
+            if (data.data.length == 0) setLoading(false);
 
+            data.data.map((e) => {
+              const id = e._id;
+              const ex = e.authorId.email;
+              const imageListRef = ref(storage, `${ex}/post/${id}`);
+              e["files"] = [];
+              setPost((prev) => [...prev, e]);
+              listAll(imageListRef).then((response) => {
+                console.log(e.content, response);
 
-  const handleDelete = async (e)=>{
+                if (response.items.length == 0) setLoading(false);
 
-    
+                response.items.forEach((item) => {
+                  getDownloadURL(item).then((url) => {
+                    setLoading(false);
 
-    axios.post("http://localhost:4000/api/delete-post",{postId:e._id}).then((data)=>{
-
-    toast.success("Post Deleted");
-
-
-    const storage = getStorage();
-    e.files.map((file)=>{
-
-    const desertRef = ref(storage, file);
-    deleteObject(desertRef)
-      .then((s) => {
-      
+                    setPost((current) =>
+                      current.map((obj) => {
+                        if (obj == e) {
+                          obj.files = [...obj.files, url];
+                        }
+                        return obj;
+                      })
+                    );
+                  });
+                });
+              });
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       })
-      .catch((error) => {
-        console.log(error);
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleDelete = async (e) => {
+    axios
+      .post("http://localhost:4000/api/delete-post", { postId: e._id })
+      .then((data) => {
+        toast.success("Post Deleted");
+
+        const storage = getStorage();
+        e.files.map((file) => {
+          const desertRef = ref(storage, file);
+          deleteObject(desertRef)
+            .then((s) => {})
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+
+        setPost(post.filter((a) => a !== e));
+      })
+      .catch((err) => {
+        toast.error(err.message);
       });
+  };
 
-    })
-
-
-
-    setPost(post.filter((a) => a !== e));
-
-
-    }).catch((err)=>{
-      toast.error(err.message);
-    })
-
-
-  }
-
-  const handleLike = async (e,post,user) =>{
-    
-
-    try{
-     const d =await  axios.post("http://localhost:4000/api/update-like",{post,user});
-     if( e.likes.some(obj=>obj._id == profile))
-     {
-
-       for(var i = 0;i<e.likes.length;i++)
-       {
-        if (Object.values(e.likes[i]).indexOf(profile) > -1) {
-          e.likes.splice(i,1);
-       }
-       }
-     }
-     else{
-     
-      
-
-      e.likes.push(profileD);
-  
-
-     }
-     setFlag(!flag);
-     
-    }catch(err){
+  const handleLike = async (e, post, user) => {
+    try {
+      const d = await axios.post("http://localhost:4000/api/update-like", {
+        post,
+        user,
+      });
+      if (e.likes.some((obj) => obj._id == profile)) {
+        for (var i = 0; i < e.likes.length; i++) {
+          if (Object.values(e.likes[i]).indexOf(profile) > -1) {
+            e.likes.splice(i, 1);
+          }
+        }
+      } else {
+        e.likes.push(profileD);
+      }
+      setFlag(!flag);
+    } catch (err) {
       console.log(err);
       toast.error(err.message);
     }
+  };
 
-
-    
-
-  }
-
-
-  const showLike = (arr)=>{
+  const showLike = (arr) => {
     console.log("Hover");
-    for(var i = 0;i<arr.length;i++)
-    console.log(arr[i].name);
-  }
-
+    for (var i = 0; i < arr.length; i++) console.log(arr[i].name);
+  };
 
   const [active, setActive] = useState(false);
 
@@ -242,7 +207,7 @@ function Newsfeed() {
           return (
             <div className="PostDiv">
               <div className="row">
-                <div className="col AvatarDiv">
+                <div className="col-auto AvatarDiv">
                   <div className="row mb-1">
                     <div className="col-auto">
                       <Avatar sx={{ width: 45, height: 45 }} />
@@ -267,17 +232,21 @@ function Newsfeed() {
                   </div>
                 </div>
                 <div className="col DelDiv">
-
-                {e.authorId._id == profile ? <DeleteIcon
-                    onClick={()=>{handleDelete(e)}}
-                    fontSize="large"
-                    style={{
-                      color: "#e32636",
-                      float: "right",
-                      cursor: "pointer",
-                    }}
-                  />:<></>}
-                  
+                  {e.authorId._id == profile ? (
+                    <DeleteIcon
+                      onClick={() => {
+                        handleDelete(e);
+                      }}
+                      fontSize="large"
+                      style={{
+                        color: "#e32636",
+                        float: "right",
+                        cursor: "pointer",
+                      }}
+                    />
+                  ) : (
+                    <></>
+                  )}
                 </div>
               </div>
 
@@ -295,29 +264,54 @@ function Newsfeed() {
                         className="PostImg"
                         image={image}
                         onLoad={handleLoad}
+                        style={{ marginRight: "10px" }}
                       />
                     </SwiperSlide>
                   ))}
                 </Swiper>
               </div>
 
-              <div
-                style={{
-                  height: "30px",
-                  width: "30px",
-                  marginLeft: "15px",
-                  marginTop: "15px",
-                }}
-              >
-              
-              {e.likes.length} <Heart isActive={flag ? e.likes.some(obj=>obj._id == profile)?true:false:e.likes.some(obj=>obj._id == profile)?true:false} onClick={()=>{handleLike(e,e._id,profile)}}
-              
-              
-              
-
-              />
-              <button onMouseEnter={()=>showLike(e.likes)}></button>
-                
+              <div className="row">
+                <div className="col-auto">
+                  <div
+                    style={{
+                      height: "30px",
+                      width: "30px",
+                      marginLeft: "15px",
+                      marginTop: "15px",
+                    }}
+                  >
+                    <Heart
+                      isActive={
+                        flag
+                          ? e.likes.some((obj) => obj._id == profile)
+                            ? true
+                            : false
+                          : e.likes.some((obj) => obj._id == profile)
+                          ? true
+                          : false
+                      }
+                      onClick={() => {
+                        handleLike(e, e._id, profile);
+                      }}
+                    />
+                  </div>
+                </div>
+                <Tooltip title="Names">
+                  <div
+                    className="col-auto"
+                    style={{
+                      marginTop: "17px",
+                      fontSize: "16px",
+                      marginLeft: "-15px",
+                      color: "#949494",
+                      cursor: "default",
+                    }}
+                    onMouseOver={() => showLike(e.likes)}
+                  >
+                    {e.likes.length}
+                  </div>
+                </Tooltip>
               </div>
 
               <div className="col CaptionDiv">
